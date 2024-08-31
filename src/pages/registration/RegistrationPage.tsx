f019b1b09590fba7ed24c6ea7a8ba26609ccecac
Lorenzo Panato <lpanato@pucrs.br>
2024-08-31 15:32:04 -0300

    feat: improve register form and apply code review suggestions

--- src/pages/registration/RegistrationPage.tsx
+++ src/pages/registration/RegistrationPage.tsx
@@ -1,5 +1,5 @@
 import { Button, TextField } from '@mui/material';
-import Logo from '../../assets/logo-ezenplo.png';
+import LogotipoEzenplo from '../../assets/logo-ezenplo.png';
 import { KeyboardReturn } from '@mui/icons-material';
 
 export default function RegistrationPage() {
@@ -7,31 +7,61 @@ export default function RegistrationPage() {
     <div className="w-full h-full flex">
       <section className="hidden xl:flex w-1/2 items-center justify-center bg-primary">
         <img
-          src={Logo}
-          className="max-w-96 xxl:max-w-[480px] w-full"
+          src={LogotipoEzenplo}
+          className="max-w-96 xxl:max-w-[30rem] w-full"
           alt="logo branca da ezenplo"
         />
       </section>
       <section className="w-full xl:w-1/2 p-10 xl:p-0 text-zinc-700 flex flex-col items-center justify-center">
-        <div className="max-w-[540px] xl:max-w-[480px] w-full flex flex-col items-center">
+        <div className="max-w-[34rem] xl:max-w-[30rem] xxl:max-w-[34rem] w-full flex flex-col items-center">
           <div className="w-full flex justify-end mb-4">
             <KeyboardReturn className="cursor-pointer" />
           </div>
 
           <h1 className="font-bold text-[2.2rem] md:text-[2.6rem] mb-4 xl:mb-8">Cadastro</h1>
 
-          <div className="w-full flex flex-col items-center gap-6">
-            <TextField label="CNPJ" variant="standard" className="w-full" />
-            <TextField label="Nome Fantasia" variant="standard" className="w-full" />
-            <TextField label="E-mail" variant="standard" className="w-full" />
-            <TextField label="Senha" variant="standard" className="w-full" />
-            <TextField label="Repita sua senha" variant="standard" className="w-full" />
+          <form className="w-full flex flex-col items-center gap-6">
+            <TextField
+              label="CNPJ"
+              variant="standard"
+              className="w-full"
+              placeholder="digite o CNPJ a ser cadastrado"
+              required
+            />
+            <TextField
+              label="Nome Fantasia"
+              variant="standard"
+              className="w-full"
+              placeholder="digite o nome fantasia da empresa a ser cadastrado"
+              required
+            />
+            <TextField
+              label="E-mail"
+              variant="standard"
+              className="w-full"
+              placeholder="digite o e-mail a ser cadastrado"
+              required
+            />
+            <TextField
+              label="Senha"
+              variant="standard"
+              className="w-full"
+              placeholder="digite a senha a ser cadastrada"
+              required
+            />
+            <TextField
+              label="Repita sua senha"
+              variant="standard"
+              className="w-full"
+              placeholder="repita a senha a ser cadastrada"
+              required
+            />
 
             <Button
               variant="contained"
               style={{
-                marginTop: '40px',
-                padding: '9px',
+                marginTop: '2.5rem',
+                padding: '0.6rem',
                 fontFamily: 'inherit',
                 textTransform: 'none',
                 fontSize: '1rem'
@@ -40,7 +70,7 @@ export default function RegistrationPage() {
             >
               Cadastrar
             </Button>
-          </div>
+          </form>
         </div>
       </section>
     </div>
