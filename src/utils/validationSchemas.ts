49c8726bbfd3e69aa80a04ae10d6728e647f79be
Lorenzo <lorenzohpanato@gmail.com>
2024-09-02 18:25:39 -0300

    fix: apply changes suggested during code review

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -5,26 +5,19 @@ export const hrManagerValidation: ZodType<IHRManager> = z
   .object({
     cnpj: z
       .string()
-      .nonempty('Campo obrigatório.')
+      .min(1, 'Campo obrigatório.')
       .transform((value) => value.replace(/[^\d]/g, ''))
-      .refine((value) => value.length === 14, {
-        message: 'O CNPJ deve ter exatamente 14 dígitos.'
-      }),
-    name: z.string().nonempty('Campo obrigatório.'),
-    email: z
-      .string()
-      .nonempty('Campo obrigatório.')
-      .email({ message: 'E-mail inválido.' })
-      .nonempty('Campo obrigatório.'),
+      .refine((value) => value.length === 14, 'O CNPJ deve ter exatamente 14 dígitos.'),
+    name: z.string().min(1, 'Campo obrigatório.'),
+    email: z.string().min(1, 'Campo obrigatório.').email('E-mail inválido.'),
     password: z
       .string()
-      .nonempty('Campo obrigatório.')
+      .min(1, 'Campo obrigatório.')
       .min(8, 'A senha deve ter no mínimo 8 caracteres.'),
     repeatPassword: z
       .string()
-      .nonempty('Campo obrigatório.')
+      .min(1, 'Campo obrigatório.')
       .min(8, 'A senha deve ter no mínimo 8 caracteres.')
-      .nonempty('Campo obrigatório.')
   })
   .refine((data) => data.password === data.repeatPassword, {
     message: 'As senhas devem ser iguais.',
