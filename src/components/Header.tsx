087321da633d3980a2941a085bdb64228cc6bdf0
caio.batista <caio.araujo@edu.pucrs.br>
2024-09-08 21:56:14 -0300

    refac(US-002): remove options

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,17 +18,14 @@ function Header({ headerOptions }: HeaderProps) {
       <img src={LogotipoEzenplo} className="w-auto h-9 ml-40" alt="LogotipoEzenplo" />
       <nav className="flex mr-24">
         <ul className="flex">
-          {headerOptions.map((option, index) => (
+          {headerOptions.map(({ title, to, icon }, index) => (
             <li
               key={index}
               className={`flex items-center ${index !== headerOptions.length - 1 ? 'mr-12' : 'ml-32'}`}
             >
-              <Link
-                to={option.to}
-                className="text-black flex items-center space-x-2"
-              >
-                <span>{option.icon}</span>
-                <span>{option.title}</span>
+              <Link to={to} className="text-black flex items-center space-x-2">
+                <span>{icon}</span>
+                <span>{title}</span>
               </Link>
             </li>
           ))}
@@ -36,6 +33,6 @@ function Header({ headerOptions }: HeaderProps) {
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
--- src/config/HeaderOptions.tsx
+++ src/config/HeaderOptions.tsx
@@ -1,16 +1,23 @@
-import { Logout, SupervisorAccountOutlined, FileDownloadOutlined, FileUploadOutlined } from '@mui/icons-material';
+import {
+  Logout,
+  SupervisorAccountOutlined,
+  FileDownloadOutlined,
+  FileUploadOutlined
+} from '@mui/icons-material';
 
 export const defaultHeaderOptions = {
-    logoutOnly: [
-        { title: '', to: '/logout', icon: <Logout /> }
-    ],
-    userManagement: [
-        { title: 'Gerenciamento Usuários', to: '/user-management', icon: <SupervisorAccountOutlined /> },
-        { title: '', to: '/logout', icon: <Logout /> }
-    ],
-    csvManagement: [
-        { title: 'Download Modelo', to: '/csv/download-csv', icon: <FileDownloadOutlined /> },
-        { title: 'Upload Modelo', to: '/csv/upload-csv', icon: <FileUploadOutlined /> },
-        { title: '', to: '/logout', icon: <Logout /> }
-    ]
+  logoutOnly: [{ title: '', to: '/logout', icon: <Logout /> }],
+  userManagement: [
+    {
+      title: 'Gerenciamento Usuários',
+      to: '/user-management',
+      icon: <SupervisorAccountOutlined />
+    },
+    { title: '', to: '/logout', icon: <Logout /> }
+  ],
+  csvManagement: [
+    { title: 'Download Modelo', to: '/csv/download-csv', icon: <FileDownloadOutlined /> },
+    { title: 'Upload Modelo', to: '/csv/upload-csv', icon: <FileUploadOutlined /> },
+    { title: '', to: '/logout', icon: <Logout /> }
+  ]
 };
