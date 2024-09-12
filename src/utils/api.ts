5dd551d1515867d730b1726e0f76f24281cd582f
Nicholas Luz <nicholas.s.luz@gmail.com>
2024-09-11 21:56:15 -0300

    add authorization and authentication with jwt, add axios to intercept requisitions to add request/response stuff

---
+++ src/utils/api.ts
@@ -0,0 +1,68 @@
+import axios from 'axios';
+import { isTokenExpired } from './jwt-decoder';
+import { refreshAccessToken, redirectToLogin } from '../services/auth/authService';
+
+const api = axios.create({
+  baseURL: import.meta.env.VITE_API_BASE_URL,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
+
+api.interceptors.request.use(
+  async (config) => {
+    const token = localStorage.getItem('accessToken');
+
+    if (token) {
+      if (config.url !== '/login') {
+        if (isTokenExpired(token)) {
+          try {
+            const newAccessToken = await refreshAccessToken();
+            config.headers.Authorization = `Bearer ${newAccessToken}`;
+          } catch (error) {
+            console.error('Failed to refresh token:', error);
+            redirectToLogin();
+          }
+        } else {
+          config.headers.Authorization = `Bearer ${token}`;
+        }
+      }
+    }
+
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response.status === 403) {
+      console.error('Permission denied:', error.response.data.detail);
+      return Promise.reject(error);
+    }
+
+    if (error.response.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        const newAccessToken = await refreshAccessToken();
+        originalRequest.headers.Authorization = `Bearer ${newAccessToken}`;
+        return api(originalRequest);
+      } catch (error) {
+        console.error('Failed to refresh token:', error);
+        redirectToLogin();
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
+export default api;
