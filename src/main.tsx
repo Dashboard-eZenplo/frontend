eee0c4013cbedc37b590cd89a784fd7414bb2564
martinbianchoeduardo <emiliekim09@gmail.com>
2024-09-24 23:30:34 -0300

    fix routing and add new 404 default page

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,19 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './pages/App.tsx';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from '@emotion/react';
 import customTheme from './styles/themes/customTheme.ts';
 import { AuthProvider } from './contexts/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
-  <ThemeProvider theme={customTheme}>
-    <StrictMode>
-      <AuthProvider>
-        <App />
-      </AuthProvider>
-    </StrictMode>
-  </ThemeProvider>
+  <StrictMode>
+    <ThemeProvider theme={customTheme}>
+      <BrowserRouter>
+        <AuthProvider>
+          <App />
+        </AuthProvider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </StrictMode>
 );
