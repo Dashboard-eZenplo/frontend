9829161dc014bd34d8b8c4121e82f05e4331d352
Nicholas Luz <nicholas.s.luz@gmail.com>
2024-10-29 21:52:55 -0300

    remove unnecessary url

--- src/services/managers/managerService.ts
+++ src/services/managers/managerService.ts
@@ -1,10 +1,8 @@
 import api from '../../utils/api';
 
-const API_URL = import.meta.env.VITE_API_BASE_URL;
-
 export const getManagers = async () => {
   try {
-    const response = await api.get(`${API_URL}/user/list_managers/`);
+    const response = await api.get(`/user/list_managers/`);
 
     return response.data;
   } catch (error: any) {
@@ -17,7 +15,7 @@ export const getManagers = async () => {
 
 export const deleteManager = async (id: number) => {
   try {
-    await api.delete(`${API_URL}/user/${id}`);
+    await api.delete(`/user/${id}`);
   } catch (error: any) {
     throw new Error(error.response?.data?.message || 'Error deleting manager');
   }
